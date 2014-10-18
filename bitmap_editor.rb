5a056ae771f773ece4c2f22a1e03899a4e8ea0a8
Viktor Nonov <vnonov@gmail.com>
2014-10-18 09:57:29 +0300

    adds commands' templates and basic line interpreting

--- bitmap_editor.rb
+++ bitmap_editor.rb
@@ -1,2 +1,43 @@
 class BitmapEditor
+  COMMANDS = [{name: "I", params: [/\d+/, /\d+/], action: :create },
+              {name: "C", params: [], action: :clear },
+              {name: "L", params: [/\d+/, /\d+/, /\w/],
+                          action: :color_pixel },
+              {name: "V", params: [/\d+/, /\d+/, /\d+/, /\w/],
+                          action: :draw_vertical_segment },
+              {name: "H", params: [/\d+/, /\d+/, /\d+/, /\w/],
+                          action: :draw_horizontal_segment },
+              {name: "F", params: [/\d+/, /\d+/, /\w/],
+                          action: :fill_region },
+              {name: "S", params: [], action: :show },
+              {name: "X", params: [], action: :terminate }]
+
+  def initialize
+  end
+
+  def read_lines
+    while line = STDIN.gets
+      break if line == "X\n"
+    end
+  end
+
+  def interpret_line(line)
+    chars = line.gsub(/s+/m, ' ').strip.split(' ')
+    COMMANDS.each do |command|
+      if chars[0] == command[:name]
+        if chars.count - 1 == command[:params].count
+          command[:params].each_with_index do |param, i|
+            return {status: :failed, msg: "Command #{command[:name]} doesn't match parameters type"} unless chars[i+1].match(param)
+          end
+          return { status: :success }
+        else
+          return {status: :failed, msg: "Command #{command[:name]} has #{command[:params].count} parameters"}
+        end
+      end
+    end
+    return { status: :failed, msg: "not recognized command" }
+  end
 end
+
+#editor = BitmapEditor.new
+#editor.read_lines
--- test/test_bitmap_editor.rb
+++ test/test_bitmap_editor.rb
@@ -2,4 +2,22 @@ require_relative "../bitmap_editor"
 require "test/unit"
 
 class TestBitmapEditor < Test::Unit::TestCase
+  def test_interpret_line_exiting_command
+    assert_equal({:status => :success}, BitmapEditor.new.interpret_line("I 1 2"))
+  end
+
+  def test_interpret_line_non_exiting_command
+    assert_equal({:status => :failed,
+                  :msg => "not recognized command"}, BitmapEditor.new.interpret_line("W 1 2"))
+  end
+
+  def test_interpret_line_wrong_params_count
+    assert_equal({:status => :failed,
+                  :msg => "Command I has 2 parameters"}, BitmapEditor.new.interpret_line("I 1 2 2"))
+  end
+
+  def test_interpret_line_wrong_type_params
+    assert_equal({:status => :failed,
+                  :msg => "Command I doesn't match parameters type"}, BitmapEditor.new.interpret_line("I 1 S"))
+  end
 end
